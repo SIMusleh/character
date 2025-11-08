28c38eb9594800eee130b78e9c1bcb20113ed217
SIMusleh <128906425+SIMusleh@users.noreply.github.com>
2025-11-08 02:35:43 +0000

    add header for character.h

---
+++ character.h
@@ -0,0 +1,11 @@
+/* Program name: character.h
+ * Author: Sahar Musleh
+ * Date last updated: 11/07/25
+ * Purpose: Define RPG character types using shared traits and labels */
+
+ #ifndef CHARACTER_H
+ #define CHARACTER_H
+
+ #include <string>
+
+ 
