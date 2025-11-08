e388d43b61c48bd69a1d32272d22ec154780b103
SIMusleh <128906425+SIMusleh@users.noreply.github.com>
2025-11-08 02:44:43 +0000

    add enums

--- character.h
+++ character.h
@@ -1,11 +1,26 @@
 /* Program name: character.h
  * Author: Sahar Musleh
  * Date last updated: 11/07/25
- * Purpose: Define RPG character types using shared traits and labels */
+ * Purpose: Define RPG character types using shared traits and labels
+ */
 
  #ifndef CHARACTER_H
  #define CHARACTER_H
 
  #include <string>
 
- 
+//Enum for character class types
+ enum CharacterClass { FIGHTER, ROGUE, MAGICIAN, CLERIC };
+
+ class Character {
+    protected:
+        std::string name;
+        double hp;
+        double mp;
+        int strength;
+        int dexterity;
+        int intelligence;
+        int speed;
+        int endurance;
+        int faith;
+ }
