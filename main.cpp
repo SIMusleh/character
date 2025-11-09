5f11f0f86dd3ba91b327d4534e09618765887e03
SIMusleh <128906425+SIMusleh@users.noreply.github.com>
2025-11-09 04:16:24 +0000

    add Rogue steals from fighter

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 /* Program name: main.cpp
  * Author: Sahar Musleh
  * Date last updated: 11/8/2025
@@ -7,3 +6,32 @@
 
 #include "character.h"
 #include <iostream>
+
+int main() {
+    std::string fighterName, rogueName, magicianName, clericName;
+
+    std::cout << "What is your fighter's name: ";
+    std::getline(std::cin, fighterName);
+    Fighter fighter(fighterName);
+
+    std::cout << "What is your rogue's name: ";
+    std::getline(std::cin, rogueName);
+    Rogue rogue(rogueName);
+
+    std::cout << "What is your magician's name: ";
+    std::getline(std::cin, magicianName);
+    Magician magician(magicianName);
+
+    std::cout << "What is your cleric's name: ";
+    std::getline(std::cin, clericName);
+    Cleric cleric(clericName);
+
+// Rogue steals from Fighter
+    std::cout << rogue.getName() << " Steal " << fighter.getName() << "\n";
+    rogue.steal(fighter);
+
+    std::cout << "Updated Rogue Stats\n";
+    std::cout << rogue.toString() << "\n";
+
+    std::cout << "Updated Fighter Stats\n";
+    std::cout << fighter.toString() << "\n";
