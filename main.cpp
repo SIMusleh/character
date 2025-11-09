b3874b1eb9c42ffadb29a96f8786947a82e61aad
SIMusleh <128906425+SIMusleh@users.noreply.github.com>
2025-11-09 03:27:26 +0000

    add header for main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+/* Program name: main.cpp
+ * Author: Sahar Musleh
+ * Date last updated: 11/8/2025
+ * Purpose: Main program to demonstrate RPG character classes and their abilities
+ */
+
+#include "character.h"
+#include <iostream>
