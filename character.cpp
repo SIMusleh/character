a02799e5155842da546d773a4af18eb67e4ffb12
SIMusleh <128906425+SIMusleh@users.noreply.github.com>
2025-11-09 03:24:49 +0000

    add header for character.cpp

---
+++ character.cpp
@@ -0,0 +1,11 @@
+/* Program name: character.cpp
+ * Author: Sahar Musleh
+ * Date last updated: 11/8/2025
+ * Purpose: Implementation of RPG character classes and their abilities
+ */
+
+ #include "character.h"
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ #include <sstream>
